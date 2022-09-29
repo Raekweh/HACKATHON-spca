c03d681988a4bd378630c3740fbef48d2d9b2e65
Raymond Li <raekweh@Raymonds-MacBook-Air-2.local>
2022-09-30 12:37:27 +1300

    retrieving data from mongodb

--- models/Clinic.js
+++ models/Clinic.js
@@ -1,34 +1,38 @@
 const mongoose = require('mongoose');
 
 const ClinicSchema = new mongoose.Schema({
-    region: {
-      type: String,
-      required: true
-    },
-    address: {
-      type: String,
-      required: true
-    },
-    country: {
-      type: String,
-      required: true
-    },
-    phoneNumber: {
-      type: String,
-      required: true
-    },
-    openingHours: {
-      type: String,
-      required: true
-    },
-    suburb: {
-      type: String,
-      required:true
-    },
-    specialistType: {
-      type: String,
-      required:true
-    }
-  });
+  id: {
+    type: Number,
+    required: true,
+  },
+  region: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  openingHours: {
+    type: String,
+    required: true
+  },
+  suburb: {
+    type: String,
+    required: true
+  },
+  specialistType: {
+    type: String,
+    required: true
+  }
+});
 
-module.exports = Clinic = mongoose.model('clinic', ClinicSchema);
+module.exports = Clinic = mongoose.model('clinics', ClinicSchema);
--- routes/api/clinics.js
+++ routes/api/clinics.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // Load Book model
-const Book = require('../../models/Clinic');
+const Clinic = require('../../models/Clinic');
 
 // @route GET api/books/test
 // @description tests books route
 // @access Public
 router.get('/test', (req, res) => res.send('Clinics route testing!'));
 
+//Router retriveve clincs
+router.get('/getbooks', (req, res) => {
+    Clinic.find()
+        .then(clinic => res.json(clinic))
+        .catch(err => res.status(404).json({ noclinicfound: 'No Clinic found' }));
+});
 module.exports = router;
