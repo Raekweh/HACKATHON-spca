8a7427cd320474e8eb2af79512bb198758a19432
Raekweh <Raekweh009@gmail.com>
2022-09-30 20:49:59 +1300

    updating router /getclinics

--- routes/api/clinics.js
+++ routes/api/clinics.js
@@ -10,7 +10,7 @@ const Clinic = require('../../models/Clinic');
 router.get('/test', (req, res) => res.send('Clinics route testing!'));
 
 //Router retriveve clincs
-router.get('/getbooks', (req, res) => {
+router.get('/getclinics', (req, res) => {
     Clinic.find()
         .then(clinic => res.json(clinic))
         .catch(err => res.status(404).json({ noclinicfound: 'No Clinic found' }));
