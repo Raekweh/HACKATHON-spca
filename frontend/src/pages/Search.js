23e906a75430c23f8448db5186661565a05c5118
kurtdonaldson <89885417+kurtdonaldson@users.noreply.github.com>
2022-09-30 22:12:14 +1300

    responsiveness to home and search pages

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,30 +1,27 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import clinics from '../dummyData/clinics.json';
+import clinics from "../dummyData/clinics.json";
 import Card2 from "../components/Card2";
-import {useState} from "react";
+import { useState } from "react";
 import { Grid, Container } from "@mui/material";
-import {useRef} from 'react'
+import { useRef } from "react";
 
 const Search = () => {
-  const [query,setQuery] = useState("");
+  const [query, setQuery] = useState("");
   const searchRef = useRef();
 
- 
-
   const search = (data) => {
-
-    const filtered = data.filter((clinic) =>
-    clinic.location.toLowerCase().includes(query) ||
-    clinic.name.toLowerCase().includes(query) ||
-    clinic.suburb.toLowerCase().includes(query)
-    )
-    if (query === ""){
+    const filtered = data.filter(
+      (clinic) =>
+        clinic.location.toLowerCase().includes(query) ||
+        clinic.name.toLowerCase().includes(query) ||
+        clinic.suburb.toLowerCase().includes(query)
+    );
+    if (query === "") {
       return [];
     }
     return filtered;
-  
-  }
+  };
 
   const handleScroll = (elementRef) => {
     window.scrollTo({
@@ -33,37 +30,46 @@ const Search = () => {
     });
   };
 
-  
   return (
-    <div >
-      <button className ='scrollBtn'onClick = {() => handleScroll(searchRef)}>Back To Search</button>
+    <div>
+      <button className="scrollBtn" onClick={() => handleScroll(searchRef)}>
+        Back To Search
+      </button>
 
       <div>
-      <p className = {'searchTitle ' + ((search(clinics).length > 0 ? 'translate' : ''))}>
-        Find A Local Vet
-      </p>
+        <p
+          className={
+            "searchTitle " + (search(clinics).length > 0 ? "translate" : "")
+          }
+        >
+          Find A Local Vet
+        </p>
       </div>
 
-      <div ref = {searchRef}>
-        <input type='text' placeholder="Search...." className={'search'} onChange={(e)=> setQuery(e.target.value)}
-            
-          />
-        </div>
+      <div ref={searchRef}>
+        <input
+          type="text"
+          placeholder="Search...."
+          className={"search"}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
 
-
-        <div>
-          <Grid container sx={{gridRowGap:'6vh',}}>
-            {search(clinics).map((clinic)=> {
-              return <Grid items md={3}><Card2 data={clinic}/></Grid>
-            })}
-
-          </Grid>
-
-        </div>
-        
-      
-
-      
+      <div>
+        <Grid
+          className="card-grid-container"
+          container
+          sx={{ gridRowGap: "6vh" }}
+        >
+          {search(clinics).map((clinic) => {
+            return (
+              <Grid items md={3}>
+                <Card2 data={clinic} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
     </div>
   );
 };
